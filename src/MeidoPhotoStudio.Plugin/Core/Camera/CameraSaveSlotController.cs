habeebweeb/COM3D2.MeidoPhotoStudio

commit 3a1543cece126cb8001edc19b58296821b02f5c9

Fix camera errors in VR mode

src/MeidoPhotoStudio.Plugin/Core/Camera/CameraSaveSlotController.cs
@@ -86,17 +86,9 @@ public class CameraSaveSlotController : IEnumerable<CameraInfo>
         MainCamera.StopAll();
     }
 
-    public IEnumerator<CameraInfo> GetEnumerator()
-    {
-        cameraInfoSelectList.Current = MainCamera.GetCameraInfo();
+    public IEnumerator<CameraInfo> GetEnumerator() =>
+        cameraInfoSelectList.GetEnumerator();
 
-        return ((IEnumerable<CameraInfo>)cameraInfoSelectList).GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        cameraInfoSelectList.Current = MainCamera.GetCameraInfo();
-
-        return ((IEnumerable)cameraInfoSelectList).GetEnumerator();
-    }
+    IEnumerator IEnumerable.GetEnumerator() =>
+        cameraInfoSelectList.GetEnumerator();
 }

src/MeidoPhotoStudio.Plugin/Core/Camera/CameraController.cs
@@ -31,7 +31,8 @@ public class CameraController(CustomMaidSceneService customMaidSceneService)
 
     internal void Activate()
     {
-        MainCamera.m_UOCamera.enabled = true;
+        if (MainCamera.m_UOCamera)
+            MainCamera.m_UOCamera.enabled = true;
 
         if (customMaidSceneService.OfficeScene)
             ResetCamera();

src/MeidoPhotoStudio.Plugin/Core/Camera/CameraSpeedController.cs
@@ -14,27 +14,33 @@ public class CameraSpeedController
 
     public CameraSpeedController()
     {
+        if (VRMode)
+            return;
+
         defaultMoveSpeed = UltimateOrbitCamera.moveSpeed;
         defaultZoomSpeed = UltimateOrbitCamera.zoomSpeed;
     }
 
-    public enum Speed
+    private enum Speed
     {
         Default,
         Fast,
         Slow,
     }
 
-    private UltimateOrbitCamera UltimateOrbitCamera =>
-        ultimateOrbitCamera
-            ? ultimateOrbitCamera
-            : ultimateOrbitCamera = GameMain.Instance.MainCamera.GetComponent<UltimateOrbitCamera>();
+    private static bool VRMode =>
+        GameMain.Instance.VRMode;
 
-    public void Deactivate() =>
-        ApplyDefaultSpeed();
+    private UltimateOrbitCamera UltimateOrbitCamera =>
+        VRMode ? null :
+        ultimateOrbitCamera ? ultimateOrbitCamera :
+        ultimateOrbitCamera = GameMain.Instance.MainCamera.GetComponent<UltimateOrbitCamera>();
 
     public void ApplyFastSpeed()
     {
+        if (VRMode)
+            return;
+
         if (currentCameraSpeed is Speed.Fast)
             return;
 
@@ -46,6 +52,9 @@ public class CameraSpeedController
 
     public void ApplySlowSpeed()
     {
+        if (VRMode)
+            return;
+
         if (currentCameraSpeed is Speed.Slow)
             return;
 
@@ -57,6 +66,9 @@ public class CameraSpeedController
 
     public void ApplyDefaultSpeed()
     {
+        if (VRMode)
+            return;
+
         if (currentCameraSpeed is Speed.Default)
             return;
 
@@ -65,4 +77,7 @@ public class CameraSpeedController
         UltimateOrbitCamera.moveSpeed = defaultMoveSpeed;
         UltimateOrbitCamera.zoomSpeed = defaultZoomSpeed;
     }
+
+    internal void Deactivate() =>
+        ApplyDefaultSpeed();
 }

src/MeidoPhotoStudio.Plugin/Framework/Extensions/CameraExtensions.cs
@@ -6,7 +6,10 @@ public static class CameraExtensions
 {
     public static void StopSpin(this CameraMain camera)
     {
-        var uoCamera = camera.GetComponent<UltimateOrbitCamera>();
+        if (!camera.m_UOCamera)
+            return;
+
+        var uoCamera = camera.m_UOCamera;
 
         uoCamera.xVelocity = 0f;
         uoCamera.yVelocity = 0f;

commit c627f2261b8d22a8933abf9cfa7fc0ac7647c130

Fix camera not being saved properly
This was a regression introduced in 3a1543ce.

src/MeidoPhotoStudio.Plugin/Core/Camera/CameraSaveSlotController.cs
@@ -86,8 +86,12 @@ public class CameraSaveSlotController : IEnumerable<CameraInfo>
         MainCamera.StopAll();
     }
 
-    public IEnumerator<CameraInfo> GetEnumerator() =>
-        cameraInfoSelectList.GetEnumerator();
+    public IEnumerator<CameraInfo> GetEnumerator()
+    {
+        cameraInfoSelectList.Current = MainCamera.GetCameraInfo();
+
+        return cameraInfoSelectList.GetEnumerator();
+    }
 
     IEnumerator IEnumerable.GetEnumerator() =>
         cameraInfoSelectList.GetEnumerator();