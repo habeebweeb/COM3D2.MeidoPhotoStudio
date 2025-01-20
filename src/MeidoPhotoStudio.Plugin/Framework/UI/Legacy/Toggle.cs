57b2263bd9c914e1f7f5b0f16a77225d5750f01b
habeebweeb <40900143+habeebweeb@users.noreply.github.com>
2025-01-20 10:37:18 -0800

    Add Group class for toggles
    This is similar to ToggleGroup in the Unity GUI library. This will soon
    be used in favour of SelectionGrid.

--- src/MeidoPhotoStudio.Plugin/Framework/UI/Legacy/Toggle.cs
+++ src/MeidoPhotoStudio.Plugin/Framework/UI/Legacy/Toggle.cs
@@ -15,6 +15,8 @@ public class Toggle(GUIContent content, bool state = false) : BaseControl
     {
     }
 
+    private event EventHandler<InternalChangeEventArgs> ChangedInternally;
+
     public static LazyStyle Style { get; } = new(
         StyleSheet.TextSize,
         static () => new(GUI.skin.toggle)
@@ -62,9 +64,111 @@ public class Toggle(GUIContent content, bool state = false) : BaseControl
 
     private void SetEnabled(bool enabled, bool notify = true)
     {
+        if (enabled == value)
+            return;
+
+        var previousState = value;
+
         value = enabled;
+
+        ChangedInternally?.Invoke(this, new(notify));
+
+        if (value == previousState)
+            return;
 
         if (notify)
             OnControlEvent(EventArgs.Empty);
+    }
+
+    // TODO: Take a look at how this is initialized and used in various places and maybe rework this a little bit to be
+    // more comfortable to use. Something to notice is that I'm creating dictionaries to be able to access a particular
+    // toggle within the group.
+    public class Group : IEnumerable<Toggle>
+    {
+        private readonly List<Toggle> toggles = [];
+
+        public bool AllowSwitchOff { get; set; }
+
+        public int Count =>
+            toggles.Count;
+
+        public Toggle this[int index] =>
+            (uint)index >= toggles.Count
+                ? throw new ArgumentOutOfRangeException(nameof(index))
+                : toggles[index];
+
+        public void Add(Toggle toggle)
+        {
+            _ = toggle ?? throw new ArgumentNullException(nameof(toggle));
+
+            if (toggles.Contains(toggle))
+                return;
+
+            toggles.Add(toggle);
+
+            toggle.ChangedInternally += OnToggleChanged;
+        }
+
+        public void Add(params Toggle[] toggles)
+        {
+            foreach (var toggle in toggles)
+                Add(toggle);
+        }
+
+        public void Insert(int index, Toggle toggle)
+        {
+            if ((uint)index > toggles.Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            _ = toggle ?? throw new ArgumentNullException(nameof(toggle));
+
+            if (toggles.Contains(toggle))
+                return;
+
+            toggles.Insert(index, toggle);
+
+            toggle.ChangedInternally += OnToggleChanged;
+        }
+
+        public bool Remove(Toggle toggle)
+        {
+            _ = toggle ?? throw new ArgumentNullException(nameof(toggle));
+
+            if (!toggles.Remove(toggle))
+                return false;
+
+            toggle.ChangedInternally -= OnToggleChanged;
+
+            return toggles.Remove(toggle);
+        }
+
+        public IEnumerator<Toggle> GetEnumerator() =>
+            toggles.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+
+        private void OnToggleChanged(object sender, InternalChangeEventArgs e)
+        {
+            if (sender is not Toggle changedToggle)
+                return;
+
+            if (!toggles.Contains(changedToggle))
+                return;
+
+            if (!AllowSwitchOff && !toggles.Any(toggle => toggle.Value))
+                changedToggle.value = true;
+
+            if (!changedToggle.Value)
+                return;
+
+            foreach (var toggle in toggles.Where(toggle => toggle != changedToggle))
+                toggle.SetEnabled(false, e.Notify);
+        }
+    }
+
+    private class InternalChangeEventArgs(bool notify) : EventArgs
+    {
+        public bool Notify { get; } = notify;
     }
 }
